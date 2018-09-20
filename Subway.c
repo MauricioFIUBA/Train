1730b2bd3ac4c5b4f787e150575aeddcfff3fbb9
MauricioFIUBA <MauricioFIUBA@github.com>
2018-09-19 22:54:54 -0300

    Copiado de prueba a Subway

--- Subway.c
+++ Subway.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Subway.h"
 
-typedef struct station{
-    char line;
-    char name[50];
-    struct station *next;
-} station_t;
-#define EMPTY '0'
-#define  ERR "Error: No se pudo abrir el archivo."
-/*Constantes de soporte*/
-#define PASL "PASO POR ACA LA LINEA: "
-#define PASE "PASO POR ACA LA ESTACION: "
-/**********************/
+/*printf("LINEA: %c\tESTACION: %s\n", Line, stat->name);*/
 
-void FreeStations(station_t ***subway)
+size_t NumberLine(FILE **archive)
 {
-    int line;
+    size_t numberLine = 0;
+    char search = '#';
+    if( ((*archive) = fopen("Unix_lineas_de_subte.txt","rt")) == NULL)
+    {
+        fprintf(stderr, "%s\n", ERR);
+    }
+    while (!feof((*archive)))
+    {
+        if(search == getc((*archive)))
+        {
+            numberLine++;
+        }
+    }
+    fclose((*archive));
+    return numberLine;
+}
+
+void FreeSubway(station_t ***subway, size_t amount)
+{
+    size_t line;
     station_t *clean, *iteration;
-    for (line = 0; line < 2; line++)
+    for (line = 0; line < amount; line++)
     {
         clean = (*subway)[line];
         while(clean)
@@ -28,19 +38,19 @@ void FreeStations(station_t ***subway)
             clean = iteration;
         }
     }
+    free(*subway);
 }
-void FillStations(station_t ***subway, FILE **archive)
+void FillSubway(station_t ***subway, FILE **archive,size_t amount)
 {
     size_t i;
     int line;
-    char Line;
+    char letterLine;
     char check = EMPTY;
-    station_t *iteration;
-    (*subway)= (station_t**)malloc(sizeof(station_t*)*2);
-    for (i = 0; i < 2; i++) {
+    (*subway)= (station_t**)malloc(sizeof(station_t*)*amount);
+    for (i = 0; i < amount; i++) {
         (*subway)[i]= (station_t*)malloc(sizeof(station_t));
     }
-    if( ((*archive) = fopen("archivo.txt","rt")) == NULL)
+    if( ((*archive) = fopen("Unix_lineas_de_subte.txt","rt")) == NULL)
     {
         fprintf(stderr, "%s\n", ERR);
     }
@@ -51,37 +61,84 @@ void FillStations(station_t ***subway, FILE **archive)
             if(check == EMPTY) check=getc((*archive));
             if(check == '#')
             {
-                fscanf((*archive), "%c", &Line);
-                printf("%s%c\n", PASL,Line);
+                fscanf((*archive), "%c", &letterLine);
+                printf("%s%c\n", PASL,letterLine);
                 while(getc((*archive)) != '\n' && !feof((*archive)));
             }
             check=getc((*archive));
-            for (iteration = (*subway)[line] ; check != '#' && !feof((*archive)) ; )
-            {
-                if(check == '*')
-                {
-                    fgets((*subway)[line]->name, 50, (*archive));
-
-                    printf("%s%s", PASE,(*subway)[line]->name);
-                    iteration->next = (station_t*)malloc(sizeof(station_t));
-                    iteration = iteration->next;
-                }
-                check=getc((*archive));
-            }
-            iteration->next = NULL;
-            /*printf("LINEA: %c\tESTACION: %s\n", Line, stat->name);*/
+            FillStations(&((*subway)[line]), archive, &check, letterLine);
             printf("%i\n", feof((*archive)));
         }
+        fclose((*archive));
     }
+}
+void FillStations(station_t **subway, FILE **archive, char *check, char letterLine)
+{
+    station_t *iteration;
+    for (iteration = (*subway) ; (*check) != '#' && !feof((*archive)) ; )
+    {
+        if((*check) == '*')
+        {
+            fgets(iteration->name, 50, (*archive));
+            /*
+            UnixCorrection();*/
+            printf("%s%s", PASE,iteration->name);
+            iteration->line = letterLine;
+            iteration->next = (station_t*)malloc(sizeof(station_t));
+            iteration = iteration->next;
+        }
+        (*check)=getc((*archive));
+    }
+    iteration->next = NULL;
+}
+
+station_t* SerchStation(station_t **subway, char line, char* station, size_t amount)
+{
+    station_t* search;
+    size_t iterator;
+    for (iterator = 0; iterator < amount && subway[iterator]->line != line; iterator++);
+    if(iterator > amount)
+    {
+        fprintf(stderr, "%s\n", ERR_SEARCH_LINE);
+    }
+    search = subway[iterator];
+    while(search && strcmp(search->name,station))
+    {
+        search = search->next;
+
+    }
+
+    return search;
 }
 int main(void)
 {
-    station_t **subway;
+    int amount;
+    station_t **subway, *buscado;
     FILE* archive;
+    char lineSeek;
+    char seek[50];
 
-    FillStations(&subway, &archive);
-    FreeStations(&subway);
-    fclose(archive);
+    amount = NumberLine(&archive);
+    printf("%i\n", amount);
+    FillSubway(&subway, &archive, amount);
+    printf("%s\n", "En que linea del subte esta buscando?:");
+    scanf("%c", &lineSeek);
+    while(getchar() != '\n');
+    printf("%s\n", "Cual es el nombre de la estacion?:");
+    fgets(seek, 50,stdin); /*Tengo que arreglar esto O arreglar el flujo de informacion superior*/
+    if((buscado = SerchStation(subway, lineSeek, seek, amount)) == NULL)
+    {
+        fprintf(stderr, "%s\n", "No se pudo encontrar la estacion buscada");
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        printf("La estacion encontrada es de la linea: %c y tienen de nombre: %s", buscado->line, buscado->name);
+    }
 
+
+
+
+    FreeSubway(&subway, amount);
     return EXIT_SUCCESS;
 }
