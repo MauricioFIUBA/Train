3ff49624fbd7901e5fce25707fef9d8214162f37
MauricioFIUBA <MauricioFIUBA@github.com>
2018-09-22 17:14:46 -0300

    Implementacion de FillCombination y SetStation

--- Subway.c
+++ Subway.c
@@ -4,11 +4,15 @@
 #include "Subway.h"
 
 /*printf("LINEA: %c\tESTACION: %s\n", Line, stat->name);*/
-
+void SetStation(station_t *station)
+{
+    station->combination1 = NULL;
+    station->combination2 = NULL;
+}
 size_t NumberLine(FILE **archive)
 {
     size_t numberLine = 0;
-    char search = '#';
+    char search = SEARCH_LINE;
     if( ((*archive) = fopen("Unix_lineas_de_subte.txt","rt")) == NULL)
     {
         fprintf(stderr, "%s\n", ERR);
@@ -42,13 +46,15 @@ void FreeSubway(station_t ***subway, size_t amount)
 }
 void FillSubway(station_t ***subway, FILE **archive,size_t amount)
 {
-    size_t i;
+    size_t iteration;
     int line;
     char letterLine;
     char check = EMPTY;
     (*subway)= (station_t**)malloc(sizeof(station_t*)*amount);
-    for (i = 0; i < amount; i++) {
-        (*subway)[i]= (station_t*)malloc(sizeof(station_t));
+    for (iteration = 0; iteration < amount; iteration++)
+    {
+        (*subway)[iteration]= (station_t*)malloc(sizeof(station_t));
+        SetStation((*subway)[iteration]);
     }
     if( ((*archive) = fopen("Unix_lineas_de_subte.txt","rt")) == NULL)
     {
@@ -59,15 +65,16 @@ void FillSubway(station_t ***subway, FILE **archive,size_t amount)
         for(line = 0;!feof((*archive)); line++)
         {
             if(check == EMPTY) check=getc((*archive));
-            if(check == '#')
+
+            if(check == SEARCH_LINE)
             {
                 fscanf((*archive), "%c", &letterLine);
-                printf("%s%c\n", PASL,letterLine);
+                /*printf("%s%c\n", PASL,letterLine);*/
                 while(getc((*archive)) != '\n' && !feof((*archive)));
             }
             check=getc((*archive));
             FillStations(&((*subway)[line]), archive, &check, letterLine);
-            printf("%i\n", feof((*archive)));
+            /*printf("%i\n", feof((*archive)));*/
         }
         fclose((*archive));
     }
@@ -75,22 +82,116 @@ void FillSubway(station_t ***subway, FILE **archive,size_t amount)
 void FillStations(station_t **subway, FILE **archive, char *check, char letterLine)
 {
     station_t *iteration;
-    for (iteration = (*subway) ; (*check) != '#' && !feof((*archive)) ; )
+    for (iteration = (*subway) ; (*check) != SEARCH_LINE && !feof((*archive)) ; )
     {
-        if((*check) == '*')
+        if((*check) == SEARCH_STATION)
         {
             fgets(iteration->name, 50, (*archive));
             /*
             UnixCorrection();*/
-            printf("%s%s", PASE,iteration->name);
+            /*printf("%s%s", PASE,iteration->name);*/
             iteration->line = letterLine;
             iteration->next = (station_t*)malloc(sizeof(station_t));
+            SetStation(iteration);
             iteration = iteration->next;
         }
         (*check)=getc((*archive));
     }
     iteration->next = NULL;
 }
+
+void FillCombination(station_t ***subway,  FILE **archive, size_t amount)
+{
+    bool_t firstStation = FALSE;
+    station_t *iteration;
+    size_t itera;
+    char check = EMPTY;
+    char lineSearch, line;
+    char stationSearch[50] = "";
+    if( ((*archive) = fopen("Unix_lineas_de_subte.txt","rt")) == NULL)
+    {
+        fprintf(stderr, "%s\n", ERR);
+    }
+    else
+    {
+        while (!feof((*archive)))
+        {
+            if(check == EMPTY) check=getc((*archive)); /*Si se inicia, check debe agarrar el primer valor*/
+
+
+            /*Busco La rama que aparece en la linea, en el ejemplo que consitrui el texto esta igual (una detras de otra),
+            Pero esto no tiene que ser mandatorio como lo es ahora*/
+            if(check == SEARCH_LINE)
+            {
+                fscanf((*archive), "%c", &line);
+                for(itera = 0; itera < amount && line != (*subway)[itera]->line;itera++);
+                while(check != '\n' && !feof((*archive))) check = getc((*archive));
+
+                if(itera >= amount)
+                {
+                    printf("%s\n", "No se encontro la linea pedida");
+                }
+                check=getc((*archive));
+                if(check == '\n')
+                {
+                    check=getc((*archive));
+                }
+                iteration = (*subway)[itera];
+                firstStation = TRUE;
+            }
+
+            if(check == SEARCH_STATION)
+            {
+                while(check != '\n' && !feof((*archive))) check = getc((*archive));
+                while(check == '\n')
+                {
+                    check=getc((*archive));
+                }
+                if(!firstStation)
+                {
+                    iteration = iteration->next;
+                }
+                firstStation = FALSE;
+            }
+
+            if(check == SEARCH_COMBINATION_STATION && !feof((*archive)))
+            {
+                fgets(stationSearch, 50, (*archive));
+                check=getc((*archive));
+
+                if(check == SEARCH_COMBINATION_LINE)
+                {
+                    lineSearch =  getc((*archive));
+                    while(check != '\n' && !feof((*archive))) check = getc((*archive));
+                    while(check == '\n')
+                    {
+                        check=getc((*archive));
+                    }
+                }
+                else
+                {
+                    fprintf(stderr, "%s\n", "ERROR FATAL: Falta la linea de subte de la combinacion");
+                }
+                if(iteration->combination1 == NULL)
+                {
+                    iteration->combination1 = SerchStation((*subway), lineSearch, stationSearch, amount);
+                }
+                else if(iteration->combination2 == NULL)
+                {
+                    iteration->combination2 = SerchStation((*subway), lineSearch, stationSearch, amount);
+                }
+                else
+                {
+                    printf("%s, LINEA: %c. ESTACION: %s, COMBINACION 1: %s. COMBINACION 2: %s\n", "Error: El programa no esta preparado para que una estacion tiene mas de 2 combinaciones", iteration->line, iteration->name, iteration->combination1->name, iteration->combination2->name);
+                }
+            }
+        }
+    }
+
+
+    fclose((*archive));
+}
+
 
 station_t* SerchStation(station_t **subway, char line, char* station, size_t amount)
 {
@@ -105,7 +206,6 @@ station_t* SerchStation(station_t **subway, char line, char* station, size_t amo
     while(search && strcmp(search->name,station))
     {
         search = search->next;
-
     }
 
     return search;
@@ -113,19 +213,25 @@ station_t* SerchStation(station_t **subway, char line, char* station, size_t amo
 int main(void)
 {
     int amount;
-    station_t **subway, *buscado;
+    station_t **subway/*, *buscado*/;
     FILE* archive;
-    char lineSeek;
-    char seek[50];
+    /*char lineSeek;
+    char seek[50];*/
 
     amount = NumberLine(&archive);
-    printf("%i\n", amount);
     FillSubway(&subway, &archive, amount);
-    printf("%s\n", "En que linea del subte esta buscando?:");
+
+    FillCombination(&subway, &archive, amount);
+
+    /*BusquedaDeCaminoMasCorto()*/
+
+
+    /******Busqueda de una estacion y prueba de SerchStation (SOLO ES UNA PRUEBA)*****/
+    /*printf("%s\n", "En que linea del subte esta buscando?:");
     scanf("%c", &lineSeek);
     while(getchar() != '\n');
     printf("%s\n", "Cual es el nombre de la estacion?:");
-    fgets(seek, 50,stdin); /*Tengo que arreglar esto O arreglar el flujo de informacion superior*/
+    fgets(seek, 50,stdin);
     if((buscado = SerchStation(subway, lineSeek, seek, amount)) == NULL)
     {
         fprintf(stderr, "%s\n", "No se pudo encontrar la estacion buscada");
@@ -134,10 +240,8 @@ int main(void)
     else
     {
         printf("La estacion encontrada es de la linea: %c y tienen de nombre: %s", buscado->line, buscado->name);
-    }
-
-
-
+    }*/
+    /*************************************************************/
 
     FreeSubway(&subway, amount);
     return EXIT_SUCCESS;
